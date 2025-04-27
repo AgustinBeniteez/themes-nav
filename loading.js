b918a254aa2277174bc729569835fef555d9a561
Agustín Beníteez <128295634+AgustinBeniteez@users.noreply.github.com>
2025-04-27 19:09:50 +0200

    v 2.0.6

---
+++ loading.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.wallpaper img');
+    
+    images.forEach(img => {
+        if (img.complete) {
+            img.classList.add('loaded');
+        } else {
+            img.addEventListener('load', () => {
+                img.classList.add('loaded');
+            });
+        }
+    });
+});
